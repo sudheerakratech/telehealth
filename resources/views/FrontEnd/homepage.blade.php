189a02818e42c9dd6246ed93b252c99e43c59117
Deven Patel <dp.patel123@gmail.com>
2018-10-11 17:35:56 +0530

    remove w3 school css in about and home page

--- resources/views/FrontEnd/homepage.blade.php
+++ resources/views/FrontEnd/homepage.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <title>Telemedicine | Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    {{-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> --}}
 
     <!-- banner -->
     <div class="banner-silder">

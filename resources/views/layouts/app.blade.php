7daf71fce65908feb54fc13f880d7d74233605e0
timur-ul-hasan <hasanjaved065@gmail.com>
2019-02-02 22:46:59 +0500

    Using Fluid container.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -409,7 +409,7 @@
         <div class="col-md-12">
             @if (!Auth::guest())
                 @if (Session::get('patient_centric') !== 'y' && Session::get('patient_centric') !== 'yp' && Session::get('group_id') != '100')
-                    <div class="container">
+                    <div class="fluid-container">
                         <form class="input-group form" border="0" id="search_patient_form" role="search" action="{{ url('search_patient') }}" method="POST" style="margin-bottom:0px;" data-nosh-target="search_patient_results">
                             <input type="hidden" name="type" value="div">
                             <input type="text" class="form-control search" id="search_patient" name="search_patient" placeholder="{{ trans('nosh.search_patient') }}" style="margin-bottom:0px;" required autocomplete="off">

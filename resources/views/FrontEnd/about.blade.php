e5503619cfbae210cf8b84fd43ea5b123b7f3b40
timur-ul-hasan <hasanjaved065@gmail.com>
2019-02-03 16:20:28 +0500

    Physician cannot see make appointment button.

--- resources/views/FrontEnd/about.blade.php
+++ resources/views/FrontEnd/about.blade.php
@@ -283,7 +283,7 @@
                 </div>
                 <div class="col-md-6 w3_agileits_services_bottom_r_grid">
                     <h4>Best Physicians</h4>
-                    <p>Our platform provides patients access to Best Physicians communications through various healthcare centers.</p>
+                    <p style="color: #fff">Our platform provides patients access to Best Physicians communications through various healthcare centers.</p>
 
                 </div>
                 <div class="clearfix"> </div>
@@ -291,12 +291,12 @@
             <div class="wthree_services_bottom_left_grid">
                 <div class="col-md-6 w3_agileits_services_bottom_r_grid">
                     <h4>Video Consultation</h4>
-                    <p>Our platform is designed to video consultation portal that was connect patient and doctor.</p>
+                    <p style="color: #fff">Our platform is designed to video consultation portal that was connect patient and doctor.</p>
 
                 </div>
                 <div class="col-md-6 w3_agileits_services_bottom_l_grid">
                     <div class="agile_services_bottom_l_grid1">
-                        <img src="{{asset('images/g9.jpg')}}" alt=" " class="img-responsive" />
+                        <img src="{{asset('images/about-video-consultation.jpg')}}" alt=" " class="img-responsive" />
                         <div class="w3_service_bottom_grid_pos">
                             <h3>Quality Care</h3>
                         </div>
@@ -612,25 +612,20 @@
     <div class="stats_inner jarallax" id="stats">
         <div class="container">
             <div class="inner_w3l_agile_grids">
-                <div class="col-md-3 w3layouts_stats_left w3_counter_grid">
+                <div class="col-md-4 w3layouts_stats_left w3_counter_grid">
                     <i class="fa fa-user-md" aria-hidden="true"></i>
                     <p class="counter">120</p>
                     <h3>Physicians</h3>
                 </div>
-                <div class="col-md-3 w3layouts_stats_left w3_counter_grid1">
-                    <i class="fa fa-stethoscope" aria-hidden="true"></i>
-                    <p class="counter">165</p>
-                    <h3>Assistants</h3>
-                </div>
-                <div class="col-md-3 w3layouts_stats_left w3_counter_grid2">
+                <div class="col-md-4 w3layouts_stats_left w3_counter_grid2">
                     <i class="fa fa-wheelchair" aria-hidden="true"></i>
                     <p class="counter">563</p>
                     <h3>Patients</h3>
                 </div>
-                <div class="col-md-3 w3layouts_stats_left w3_counter_grid3">
+                <div class="col-md-4 w3layouts_stats_left w3_counter_grid3">
                     <i class="fa fa-stethoscope" aria-hidden="true"></i>
                     <p class="counter">245</p>
-                    <h3>Consultation</h3>
+                    <h3>Consultations</h3>
                 </div>
                 <div class="clearfix"> </div>
             </div>
